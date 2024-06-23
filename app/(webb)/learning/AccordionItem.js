6897356a38ee07ac06548a241a0581f7c9a5915b
nftcryptomix-admin <steevcan@gmail.com>
2024-06-24 10:02:27 +1200

    Smoother coding

---
+++ app/(webb)/learning/AccordionItem.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+
+const AccordionItem = ({ id, heading, title, description, src }) => {
+  const [isIframeLoaded, setIframeLoaded] = useState(false);
+
+  const handleToggle = () => {
+    setIframeLoaded(!isIframeLoaded);
+  };
+
+  return (
+    <div className="accordion-item">
+      <h2 className="accordion-header" id={`panelsStayOpen-heading${id}`}>
+        <button
+          className="accordion-button"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target={`#panelsStayOpen-collapse${id}`}
+          aria-expanded="true"
+          aria-controls={`panelsStayOpen-collapse${id}`}
+          onClick={handleToggle}
+        >
+          <span className="fw-2 fw-normal">{heading}</span>
+        </button>
+      </h2>
+      <div
+        id={`panelsStayOpen-collapse${id}`}
+        className="accordion-collapse collapse"
+        aria-labelledby={`panelsStayOpen-heading${id}`}
+      >
+        <div className="accordion-body ms-0 ps-0">
+          <div className="row featurette p-5 w-100">
+            <div className="col-12 text-center mb-4">
+              <h2 className="featurette-heading fw-bold lh-1">
+                Learn With Skivy <span className="text-muted fw-normal">{title}</span>
+              </h2>
+              <p className="lead">{description}</p>
+            </div>
+            <div className="col-12 d-flex justify-content-center">
+              {isIframeLoaded && (
+                <iframe
+                  src={src}
+                  allowtransparency="true"
+                  width="485"
+                  height="402"
+                  frameBorder="0"
+                  scrolling="no"
+                  allowFullScreen
+                ></iframe>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AccordionItem;
--- app/(webb)/learning/page.jsx
+++ app/(webb)/learning/page.jsx
@@ -1,27 +1,70 @@
 'use client'
-import React from 'react'
-import { useRouter } from 'next/navigation'
-
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import AccordionItem from '././AccordionItem'; // Adjust the path according to your project structure
 
 const Learning = () => {
   const router = useRouter();
-  const openContactUs=()=>{
-    router.push("/contactus")
+  const openContactUs = () => {
+    router.push("/contactus");
   };
+
+  // Accordion data for Section One
+  const accordionDataOne = [
+    { id: 'One', heading: 'Alphabet For Spelling', title: '"Alphabet For Spelling"', description: "Press the green flag and then press Learning ABC's to start the lesson, repeat after me.", src: 'https://scratch.mit.edu/projects/988205839/embed' },
+    { id: 'Two', heading: 'Learn With Skivy "Jobs"', title: '"Jobs"', description: "Press the green flag and then press Learning ABC's to start the lesson.", src: 'https://scratch.mit.edu/projects/1007851238/embed' },
+    { id: 'Three', heading: 'Learn With Skivy "Greetings"', title: '"Greetings"', description: "Press the green flag and then press Learning ABC's to start the lesson.", src: 'https://scratch.mit.edu/projects/1005347039/embed' },
+    { id: 'Four', heading: 'Learn With Skivy "Hobbies"', title: '"Hobbies"', description: "Press the green flag and then press Learning ABC's to start the lesson.", src: 'https://scratch.mit.edu/projects/1006176517/embed' },
+    { id: 'Five', heading: 'Learn With Skivy "Preposition Adventure"', title: '"Preposition Adventure"', description: "Press the green flag and then press Learning ABC's to start the lesson.", src: 'https://scratch.mit.edu/projects/1020467974/embed' },
+    { id: 'Six', heading: ' Learn With Skivy "Diagraphs"', title: '"Diagraphs"', description: "Press the green flag and then press Learning or Diagraphs to start the lesson.", src: 'https://scratch.mit.edu/projects/1029641127/embed' },
+    { id: 'Seven', heading: 'Learn With Skivy "Diagraph Quiz 1"', title: '"Diagraph Quiz 1"', description: "Press the green flag and then press Learning or Diagraphs to start the lesson.", src: 'https://scratch.mit.edu/projects/1030120626/embed' },
+    { id: 'Eight', heading: 'Learn With Skivy "Diagraph Quiz 2"', title: '"Diagraph Quiz 2"', description: "Press the green flag and then press Learning or Diagraphs to start the lesson.", src: 'https://scratch.mit.edu/projects/1030439255/embed' },
+    { id: 'Nine', heading: 'Learn With Skivy "Diagraph Quiz 3"', title: '"Diagraph Quiz 3"', description: "Press the green flag and then press Learning or Diagraphs to start the lesson.", src: 'https://scratch.mit.edu/projects/1030452781/embed' },
+    { id: 'Ten', heading: ' Learn With Skivy P1 "Two Letter Spelling Adventure"', title: 'P1 "Two Letter Spelling Adventure"', description: "Press the green flag and then press Learning or Diagraphs to start the lesson.", src: 'https://scratch.mit.edu/projects/992419130/embed' },
+    { id: 'Eleven', heading: 'Learn With Skivy P1 "Two Letter Spelling"', title: 'P1 "Two Letter Spelling"', description: "Press the green flag and then press Learning or Diagraphs to start the lesson.", src: 'https://scratch.mit.edu/projects/1013590415/embed' },
+    { id: 'Twelve', heading: 'Learn With Skivy P2 "Two Letter Spelling Adventure"', title: 'P2 "Two Letter Spelling Adventure"', description: "Press the green flag and then press Learning or Diagraphs to start the lesson.", src: 'https://scratch.mit.edu/projects/994841793/embed' },
+    { id: 'Thirteen', heading: 'Learn With Skivy  P2 "Two Letter Spelling"', title: 'P2 "Two Letter Spelling"', description: "Press the green flag and then press Learning or Diagraphs to start the lesson.", src: 'https://scratch.mit.edu/projects/1030452781/embed' },
+
+    // Add more items as needed
+  ];
+
+  // Accordion data for Section Two
+  const accordionDataTwo = [
+    { id: 'One', heading: 'Basic Math Concepts', title: '"Basic Math"', description: "Press the green flag to start the lesson on basic math concepts.", src: 'https://scratch.mit.edu/projects/1012345678/embed' },
+    { id: 'Two', heading: 'Science Basics', title: '"Science Basics"', description: "Press the green flag to start the lesson on basic science concepts.", src: 'https://scratch.mit.edu/projects/1012345679/embed' },
+    { id: 'Three', heading: 'History Overview', title: '"History Overview"', description: "Press the green flag to start the lesson on history basics.", src: 'https://scratch.mit.edu/projects/1012345680/embed' },
+    // Add more items as needed
+  ];
+
+  // Accordion data for Section Three
+  const accordionDataThree = [
+    { id: 'One', heading: 'Advanced Math Concepts', title: '"Advanced Math"', description: "Press the green flag to start the lesson on advanced math concepts.", src: 'https://scratch.mit.edu/projects/1012345681/embed' },
+    { id: 'Two', heading: 'Physics Basics', title: '"Physics Basics"', description: "Press the green flag to start the lesson on basic physics concepts.", src: 'https://scratch.mit.edu/projects/1012345682/embed' },
+    { id: 'Three', heading: 'Geography Basics', title: '"Geography Basics"', description: "Press the green flag to start the lesson on geography basics.", src: 'https://scratch.mit.edu/projects/1012345683/embed' },
+    // Add more items as needed
+  ];
+
+    // Accordion data for Section Four Learn With Skivy Book Series Challenge
+    const accordionDataFour = [
+      { id: 'One', heading: 'Advanced Math Concepts', title: '"Advanced Math"', description: "Press the green flag to start the lesson on advanced math concepts.", src: 'https://scratch.mit.edu/projects/1012345681/embed' },
+      { id: 'Two', heading: 'Physics Basics', title: '"Physics Basics"', description: "Press the green flag to start the lesson on basic physics concepts.", src: 'https://scratch.mit.edu/projects/1012345682/embed' },
+      { id: 'Three', heading: 'Geography Basics', title: '"Geography Basics"', description: "Press the green flag to start the lesson on geography basics.", src: 'https://scratch.mit.edu/projects/1012345683/embed' },
+      // Add more items as needed
+    ];
+
   return (
-  
     <div className="banner">
       <div className="w-100">
         <div className="container">
           <div className="row justify-content-evenly">
             <div className="col-lg-12 col-med-12 col-sm-12 mx-auto">
-                <h1 className="lead fw-bold text-center mt-4 pt-4 mb-3 pb-3 display-1">
-                  Learning
-                </h1>
-                <h3 className="text-center mb-3 pb-3 fst-italic">"Education Is Fun"</h3>
+              <h1 className="lead fw-bold text-center mt-4 pt-4 mb-3 pb-3 display-1">
+                Learning
+              </h1>
+              <h3 className="text-center mb-3 pb-3 fst-italic">"Education Is Fun"</h3>
               <h5 className="text-center lh-lg">This is our learning hub, you'll find a growing library of interactive Scratch lessons and activities. These fun, visually engaging projects allow children to explore a range of learning concepts, problem-solving, and creative expression at their own pace. If you have any quiries please feel free to contact us.</h5>
               <div className="mt-4 text-center">
-              <button className="btn btn-lg btn-primary fw-bold mb-3" onClick={openContactUs}>Contact Us</button>
+                <button className="btn btn-lg btn-primary fw-bold mb-3" onClick={openContactUs}>Contact Us</button>
               </div>
             </div>
           </div>
@@ -30,111 +73,98 @@ const Learning = () => {
 
       <hr className="featurette-divider mt-5 pt-1 mb-5" style={{ width: '80%', margin: '0 auto', backgroundColor: '#000', height: '2px' }} />
 
-    <div className="row justify-content-evenly">
-      <div className="col-lg-8 col-med-10 col-sm-12 mx-auto">
+      {/* Section One */}
+      <div className="row justify-content-evenly">
+        <div className="col-lg-8 col-med-10 col-sm-12 mx-auto">
           <h2 className="text-center featurette-heading fw-bold lh-1">Learn With Skivy <span className="text-muted fw-normal">"Building Blocks"</span></h2>
           <p className="text-center lead mt-4 mb-4">The "Building Blocks" are the core, essential skills and knowledge that form the foundation for everything else you'll learn.</p>
-      </div>
-
-      <div className="accordion w-75" id="accordionPanelsStayOpenExample">
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="panelsStayOpen-headingOne">
-            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-            <span className="fw-2 fw-normal">Alphabet For Spelling</span>
-            </button>
-          </h2>
-        <div id="panelsStayOpen-collapseOne" className="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
-          <div className="accordion-body ms-0 ps-0">
-            <div className="row featurette p-5 w-100">
-              <div className="col-12 text-center mb-4">
-                <h2 className="featurette-heading fw-bold lh-1">Learn With Skivy <span className="text-muted fw-normal">"Alphabet For Spelling"</span></h2>
-                <p className="lead">Press the green flag and then press Learning ABC's to start the lesson, repeat after me.</p>
-              </div>
-              <div className="col-12 d-flex justify-content-center">
-                <iframe 
-                  src="https://scratch.mit.edu/projects/988205839/embed" 
-                  allowtransparency="true" 
-                  width="485" 
-                  height="402" 
-                  frameBorder="0" 
-                  scrolling="no" 
-                  allowFullScreen
-                ></iframe>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
-      <div className="accordion-item">
-          <h2 className="accordion-header" id="panelsStayOpen-headingTwo">
-            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="true" aria-controls="panelsStayOpen-collapseTwo">
-            <span className="fw-2 fw-normal">Alphabet For Spelling</span>
-            </button>
-          </h2>
-        <div id="panelsStayOpen-collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo">
-          <div className="accordion-body ms-0 ps-0">
-            <div className="row featurette p-5 w-100">
-              <div className="col-12 text-center mb-4">
-                <h2 className="featurette-heading fw-bold lh-1">Learn With Skivy <span className="text-muted fw-normal">"Alphabet For Spelling"</span></h2>
-                <p className="lead">Press the green flag and then press Learning ABC's to start the lesson, repeat after me.</p>
-              </div>
-              <div className="col-12 d-flex justify-content-center">
-                <iframe 
-                  src="https://scratch.mit.edu/projects/988205839/embed" 
-                  allowtransparency="true" 
-                  width="485" 
-                  height="402" 
-                  frameBorder="0" 
-                  scrolling="no" 
-                  allowFullScreen
-                ></iframe>
-              </div>
-            </div>
-          </div>
+
+        <div className="accordion w-75" id="accordionPanelsStayOpenExample">
+          {accordionDataOne.map((item) => (
+            <AccordionItem
+              key={item.id}
+              id={item.id}
+              heading={item.heading}
+              title={item.title}
+              description={item.description}
+              src={item.src}
+            />
+          ))}
         </div>
       </div>
 
-      <div className="accordion-item">
-          <h2 className="accordion-header" id="panelsStayOpen-headingThree">
-            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="true" aria-controls="panelsStayOpen-collapseThree">
-            <span className="fw-2 fw-normal">Alphabet For Spelling</span>
-            </button>
-          </h2>
-        <div id="panelsStayOpen-collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree">
-          <div className="accordion-body ms-0 ps-0">
-            <div className="row featurette p-5 w-100">
-              <div className="col-12 text-center mb-4">
-                <h2 className="featurette-heading fw-bold lh-1">Learn With Skivy <span className="text-muted fw-normal">"Alphabet For Spelling"</span></h2>
-                <p className="lead">Press the green flag and then press Learning ABC's to start the lesson, repeat after me.</p>
-              </div>
-              <div className="col-12 d-flex justify-content-center">
-                <iframe 
-                  src="https://scratch.mit.edu/projects/988205839/embed" 
-                  allowtransparency="true" 
-                  width="485" 
-                  height="402" 
-                  frameBorder="0" 
-                  scrolling="no" 
-                  allowFullScreen
-                ></iframe>
-              </div>
-            </div>
-          </div>
+      <hr className="featurette-divider mt-5 pt-1 mb-5" style={{ width: '80%', margin: '0 auto', backgroundColor: '#000', height: '2px' }} />
+
+      {/* Section Two */}
+      <div className="row justify-content-evenly">
+        <div className="col-lg-8 col-med-10 col-sm-12 mx-auto">
+          <h2 className="text-center featurette-heading fw-bold lh-1">Learn With Skivy <span className="text-muted fw-normal">"Growing Strong"</span></h2>
+          <p className="text-center lead mt-4 mb-4">Building upon the basics to enhance learning.</p>
+        </div>
+
+        <div className="accordion w-75" id="accordionPanelsStayOpenExample2">
+          {accordionDataTwo.map((item) => (
+            <AccordionItem
+              key={item.id}
+              id={item.id}
+              heading={item.heading}
+              title={item.title}
+              description={item.description}
+              src={item.src}
+            />
+          ))}
         </div>
       </div>
-</div>
-</div>
 
-<hr className="featurette-divider mt-5 pt-1 mb-5" style={{ width: '80%', margin: '0 auto', backgroundColor: '#000', height: '2px' }} />
+      <hr className="featurette-divider mt-5 pt-1 mb-5" style={{ width: '80%', margin: '0 auto', backgroundColor: '#000', height: '2px' }} />
 
+      {/* Section Three */}
+      <div className="row justify-content-evenly">
+        <div className="col-lg-8 col-med-10 col-sm-12 mx-auto">
+          <h2 className="text-center featurette-heading fw-bold lh-1">Learn With Skivy <span className="text-muted fw-normal">"Mastering the Challenge"</span></h2>
+          <p className="text-center lead mt-4 mb-4">Ready for more complex concepts and skills.</p>
+        </div>
 
+        <div className="accordion w-75" id="accordionPanelsStayOpenExample3">
+          {accordionDataThree.map((item) => (
+            <AccordionItem
+              key={item.id}
+              id={item.id}
+              heading={item.heading}
+              title={item.title}
+              description={item.description}
+              src={item.src}
+            />
+          ))}
+        </div>
+      </div>
 
-<hr className="featurette-divider mt-3 mb-3" style={{ width: '80%', margin: '0 auto', backgroundColor: '#000', height: '2px' }} />
+      <hr className="featurette-divider mt-5 pt-1 mb-5" style={{ width: '80%', margin: '0 auto', backgroundColor: '#000', height: '2px' }} />
 
+      {/* Section Four */}
+      <div className="row justify-content-evenly">
+        <div className="col-lg-8 col-med-10 col-sm-12 mx-auto">
+          <h2 className="text-center featurette-heading fw-bold lh-1">Learn With Skivy <span className="text-muted fw-normal">"Book Challenge Series"</span></h2>
+          <p className="text-center lead mt-4 mb-4">Reading the Learn With Skivy Series where you will be challenged with spelling and grammar.</p>
+        </div>
+
+        <div className="accordion w-75" id="accordionPanelsStayOpenExample3">
+          {accordionDataFour.map((item) => (
+            <AccordionItem
+              key={item.id}
+              id={item.id}
+              heading={item.heading}
+              title={item.title}
+              description={item.description}
+              src={item.src}
+            />
+          ))}
+        </div>
+      </div>
 
     </div>
-    
-  )
-}
+  );
+};
 
-export default Learning
+export default Learning;

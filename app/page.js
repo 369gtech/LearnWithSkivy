6f969fb5d90ec7cb68926fd84f39decc6343f1a3
nftcryptomix-admin <steevcan@gmail.com>
2024-07-07 11:25:47 +1200

    Making the home page more mobile friendly

--- app/page.js
+++ app/page.js
@@ -5,20 +5,20 @@ export default function Home() {
     <main className="banner">
       <div className="w-100">
         <div className="container">
-          <div className="row justify-content-evenly">
-            <div className="col-lg-6 col-med-8 col-sm-12 col-12 mx-auto">
+          <div className="row justify-content-evenly align-items-center">
+            <div className="col-lg-6 col-med-8 col-sm-12 col-12 mx-auto text-center">
             <h3 className="fw-bold text-center mt-5 pt-5">Welcome To</h3>
               <h1 className="lead fw-bold text-center display-1">
                 Learn With Skivy
               </h1>
-              <h5 className="text-center fw-light fst-italic mt-3">"Igniting curiosity through interactive exploration."</h5>
-              <p className="lead text-center mt-4">
+              <h5 className="fw-light fst-italic mt-3">"Igniting curiosity through interactive exploration."</h5>
+              <p className="lead mt-4">
               <Link href="/aboutus" className="btn btn-lg btn-primary fw-bold">About Us
               </Link>
               </p>
             </div>
-            <div className="col-lg-6 col-sm-6 col-12 mx-auto">
-              <img src="/lwslogo.png" alt="banner" />
+            <div className="col-lg-6 col-md-8 col-sm-10 col-12 mx-auto text-center">
+              <img src="/lwslogo.png" alt="banner" width={300} height={300} />
             </div>
           </div>
         </div>
